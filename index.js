066ba76d051a7f65cf33bd593351b904f497a57f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-28 15:15:14 +0430

    fs module

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
 fs.readFile('./text.txt', 'utf-8', (err, data) => {
-  console.log(data);
+  fs.readFile(`./${data}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.writeFile('./final.txt', `${data} ${data2}`, 'utf-8', (err) => {
+      console.log('you file has been written');
+    });
+  });
 });
 
 console.log('will read the file!');
