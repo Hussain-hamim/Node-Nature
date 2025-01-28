2caaa9bc2f5aa9eaab8100712f589465779d2c2f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-28 15:35:16 +0430

    server

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 const http = require('http');
 
-fs.readFile('./text.txt', 'utf-8', (err, data) => {
-  fs.readFile(`./${data}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.writeFile('./final.txt', `${data} ${data2}`, 'utf-8', (err) => {
-      console.log('you file has been written');
-    });
-  });
+//////////////
+//FILE
+
+// fs.readFile('./text.txt', 'utf-8', (err, data) => {
+//   fs.readFile(`./${data}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.writeFile('./final.txt', `${data} ${data2}`, 'utf-8', (err) => {
+//       console.log('you file has been written');
+//     });
+//   });
+// });
+
+// console.log('will read the file!');
+
+//////////////
+//SERVER
+
+const server = http.createServer((req, res) => {
+  res.end('hello from the server.');
 });
 
-console.log('will read the file!');
+//          port  ,   host
+server.listen(8000, '127.0.0.1', () => {
+  console.log('listening on port 8000');
+});
