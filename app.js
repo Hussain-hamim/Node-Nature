994336aef83d807251619ea001db37324fce1603
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-29 22:58:16 +0430

    middleware

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('hello from middleware ');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
+
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 
 const getAllTour = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: { tours: tours },
   });
