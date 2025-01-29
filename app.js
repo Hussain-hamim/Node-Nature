1e1bbb2d806a4ff742383aff697442681821f833
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-29 15:28:16 +0430

    post req

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.json({ message: "hello from server", app: "nodejs" });
@@ -21,6 +22,29 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours: tours },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    './dev-data/data/tours-simple.json', // path
+    JSON.stringify(tours), // data - in this new tours we added the new tours
+    (err) => {
+      //and we send back the client this response
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  //   res.send('done');
 });
 
 const port = 3000;
