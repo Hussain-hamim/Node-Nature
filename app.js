7b477c315530ff55ac8f6d0d935a694b1c66a8ec
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-29 13:05:35 +0430

    express...

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  //
+  res.status(200).send("hello from server...");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
