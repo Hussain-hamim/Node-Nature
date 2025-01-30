b0630ab710de000d30456f8d8de338b2b4e80417
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-30 22:18:44 +0430

    serving static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 // MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static('./public'));
 
 app.use((req, res, next) => {
   console.log('hello from middleware 🙌');
