35adcd8eeb41e4ec5860a7fc53ebd1ec21bbe5c8
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-31 12:45:29 +0430

    env variables

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+console.log('ENV: ', process.env.NODE_ENV);
+// console.log('USER: ', process.env.USER);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
+  console.log('🙌');
 });
