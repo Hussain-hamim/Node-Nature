9b6954379e14c2523698fbb15c03020a05c49830
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-02-01 13:47:44 +0430

    mongodb

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
@@ -6,12 +7,28 @@ const app = require('./app');
 console.log('ENV: ', process.env.NODE_ENV);
 // console.log('USER: ', process.env.USER);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-  console.log('🙌');
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log('db connection successful! 😎 \n');
+  })
+  .catch((err) => console.log('db connection failed 😡'));
+
+const tourSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'a tour must have a name'] },
+
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'a tour must have price'],
+  },
 });
 
-const x = 24;
+const Tour = mongoose.model('Tour', tourSchema);
 
-x = 66;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server   listening on port ${port}`);
+});
