ecaace78e49afbf6600dbf76c9f89f6601c49b3c
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-02-25 11:13:02 +0430

    // a custom global error handling middleware

--- app.js
+++ app.js
@@ -30,12 +30,28 @@ app.use('/api/v1/users', userRouter);
 
 // route for all undefined routes to give error message
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server.`,
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server.`,
+  // });
 
-  next();
+  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// a custom global error handling middleware
+app.use((err, req, res, next) => {
+  // if the err object could not find any value for status and code then it will run this default value
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 module.exports = app;
