e7f9f1e3a5e693281213ab2445021cbea5fb6a0f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-02-28 12:23:34 +0430

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,9 +10,14 @@ exports.signUp = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
     data: {
+      token,
       user: newUser,
     },
   });
