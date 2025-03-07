818ec3f46200327fc15787763ccf5090800c28af
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-07 12:40:10 +0430

    add user update filtering to prevent password changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el]; // if the field is allowed then add it to the new object to be returned else ignore it and move to the next field in the object to be checked  and so on till all fields are checked
+  });
+  return newObj;
+}; // this function will filter out the fields that are not allowed to be updated  like password
+
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
@@ -21,7 +29,7 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
   //1) create error if user posts password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -33,10 +41,21 @@ exports.updateMe = (req, res, next) => {
   }
 
   //2) update user doc
+  const filteredBody = filterObj(req.body, 'name', 'email'); // this will filter out the fields that are not allowed to be updated  like password and role and so on
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  updatedUser.name = 'hussain';
+  // user.save(); cant use save here because it will not run the validators
+
   res.status(200).json({
     status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
