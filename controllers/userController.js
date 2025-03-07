8336a73fabb8783a0d0e522ae2db0c4cd212fcb8
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-07 12:20:11 +0430

    update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -19,6 +20,24 @@ exports.createUser = (req, res) => {
     message: 'this route is not yet defined',
   });
 };
+
+exports.updateMe = (req, res, next) => {
+  //1) create error if user posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'this route is not for password updates, use /updateMe route',
+        400,
+      ),
+    );
+  }
+
+  //2) update user doc
+  res.status(200).json({
+    status: 'success',
+  });
+};
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
