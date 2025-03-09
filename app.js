92fabf9f86f3eeb93c54f48f6a08a4109c9bde59
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-09 19:15:23 +0430

    add rate limiting middleware and update package names

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -9,10 +10,18 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// MIDDLEWARE
+//1) GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  WindowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour.',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static('./public'));
 
