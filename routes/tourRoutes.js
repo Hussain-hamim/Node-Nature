3275e169188640999d71a481d9a34810ea3a4cd6
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-16 16:21:02 +0430

    adding missing auth

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -25,17 +25,31 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTour);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan,
+  );
 
 router
   .route('/')
   .get(authController.protect, tourController.getAllTour)
-  .post(tourController.createTour);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour,
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour,
+  )
   .delete(
     authController.protect,
     // authController.restrictTo('admin', 'lead-guide'),
