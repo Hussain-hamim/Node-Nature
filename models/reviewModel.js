793feccf154a01789d60d6b511486555ae2a329a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-03-19 22:02:35 +0430

    reviewSchema.index

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,8 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 // tour and user populate pre hook
 reviewSchema.pre(/^find/, function (next) {
   //   this.populate({
